4f3f1822da19b877d5db87f8cbc016ac36315cb7
Anjan Roy <roy.anjan5@gmail.com>
2026-02-24 18:34:13 +0530

    changes saved

--- problem_15.py
+++
@@ -1,7 +0,0 @@
-# Find gcd
-# ----------------------
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-print(gcd(43,78))
--- problem_5.py
+++ problem_5.py
@@ -1,6 +1,1 @@
-# Sum of list
-# ----------------------
-l=[3,4,5,6]
-def sum_list(lst):
-    return sum(lst)
-print(sum_list(l))
+
